870b0f539ac1151fce810a734f2157c0e049a309
Stakhnev Yury <yury.stakhnev@gmail.com>
2025-08-18 22:54:34 +0300

    feat: create handlebars configuration for dynamic page rendering
    - Added a new handlebars.config.js file to centralize configuration for Handlebars templates.
    - Defined partDirs for component directories and pageData for dynamic page titles and active menu item highlighting.
    - Updated vite.config.js to utilize the new handlebars configuration, streamlining the setup for Handlebars integration.
    - Removed redundant pageData and partDirs definitions from vite.config.js to enhance maintainability.

--- vite.config.js
+++ vite.config.js
@@ -3,108 +3,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
+import { getHandlebarsConfig } from './handlebars.config.js';
 
 const basePath = process.env.NODE_ENV === 'development' ? '' : '/davaleather-layout';
-
-const partDirs = [
-  'src/components/header',
-  'src/components/footer',
-  'src/components/contacts',
-  'src/components/about-hero',
-  'src/components/head',
-  'src/components/placeholder',
-  'src/components/main-hero',
-  'src/components/cta-button',
-  'src/components/modal',
-  'src/components/modal-phone',
-  'src/components/modal-zipcode',
-  'src/components/modal-form',
-  'src/components/modal-success',
-  'src/components/search',
-  'src/components/help-request',
-  'src/layouts',
-];
-
-const pageData = {
-  '/index.html': {
-    title: 'Dava Leather',
-    highlightActiveMenuItems: {
-      home: true,
-    },
-  },
-  '/pages/about/index.html': {
-    title: 'About us - Dava Leather',
-    highlightActiveMenuItems: {
-      about: true,
-    },
-  },
-  '/pages/price/index.html': {
-    title: 'Price - Dava Leather',
-    highlightActiveMenuItems: {
-      price: true,
-    },
-  },
-  '/pages/residential/index.html': {
-    title: 'Residential Furniture Service - Dava Leather',
-    highlightActiveMenuItems: {
-      residential: true,
-      services: true,
-    },
-  },
-  '/pages/commercial/index.html': {
-    title: 'Commercial Furniture Repair - Dava Leather',
-    highlightActiveMenuItems: {
-      commercial: true,
-      services: true,
-    },
-  },
-  '/pages/automotive/index.html': {
-    title: 'Automotive Interior Restoration - Dava Leather',
-    highlightActiveMenuItems: {
-      automotive: true,
-      services: true,
-    },
-  },
-  '/pages/cleaning/index.html': {
-    title: 'Leather Cleaning & Protecting - Dava Leather',
-    highlightActiveMenuItems: {
-      cleaning: true,
-      services: true,
-    },
-  },
-  '/pages/blog/index.html': {
-    title: 'Blog - Dava Leather',
-    highlightActiveMenuItems: {
-      blog: true,
-    },
-  },
-
-  '/pages/blog/leather-cracks/index.html': {
-    title: 'Why leather cracks: causes and prevention - Dava Leather',
-    highlightActiveMenuItems: {
-      blog: true,
-    },
-  },
-  '/pages/blog/car-leather-dangers/index.html': {
-    title: 'Why is improper cleaning of a car’s leather interior dangerous? - Dava Leather',
-    highlightActiveMenuItems: {
-      blog: true,
-    },
-  },
-  '/pages/blog/sofa-rehab/index.html': {
-    title: 'Can a well-worn leather sofa be rehabbed? - Dava Leather',
-    highlightActiveMenuItems: {
-      blog: true,
-    },
-  },
-  '/pages/blog/fake-leather/index.html': {
-    title:
-      'Real vs. fake leather —what’s the deal & how it affects furniture repair - Dava Leather',
-    highlightActiveMenuItems: {
-      blog: true,
-    },
-  },
-};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -112,18 +13,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   root: 'src',
   base: basePath,
-  plugins: [
-    handlebars({
-      context(pagePath) {
-        return pageData[pagePath];
-      },
-      partialDirectory: partDirs,
-      reloadOnPartialChange: true,
-      helpers: {
-        basePath: (path) => `${basePath}${path}`,
-      },
-    }),
-  ],
+  plugins: [handlebars(getHandlebarsConfig(basePath))],
   css: {
     preprocessorOptions: {
       scss: {},
