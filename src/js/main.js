e0f381d30a2e2f6a7e71ae194ee98a3ba2023ab7
Stakhnev Yury <yury.stakhnev@gmail.com>
2025-08-23 19:07:58 +0300

    refactor: restructure modal components and update styles
    - Moved modal components into a new 'modals' directory for better organization.
    - Created a base modal structure with Handlebars and JavaScript functionality for improved reusability.
    - Added specific modals for phone, success, form, and zipcode with corresponding styles and functionality.
    - Removed old modal styles and scripts to streamline the codebase and enhance maintainability.
    - Updated main.js and SCSS imports to reflect the new directory structure.

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ import { initAboutHero } from '../components/about-hero/about-hero.js';
 import { initMainHero } from '../components/main-hero/main-hero.js';
 import { initContacts } from '../components/contacts/contacts.js';
 import { initFooter } from '../components/footer/footer.js';
-import { initPhoneModal } from '../components/modal-phone/modal-phone.js';
-import { initZipcodeModal } from '../components/modal-zipcode/modal-zipcode.js';
-import { initFormModal } from '../components/modal-form/modal-form.js';
-import { initSuccessModal } from '../components/modal-success/modal-success.js';
+import { initPhoneModal } from '../components/modals/modal-phone/modal-phone.js';
+import { initZipcodeModal } from '../components/modals/modal-zipcode/modal-zipcode.js';
+import { initFormModal } from '../components/modals/modal-form/modal-form.js';
+import { initSuccessModal } from '../components/modals/modal-success/modal-success.js';
 import { initHelpRequest } from '../components/help-request/help-request.js';
 import { initReviews } from '../components/reviews/reviews.js';
 
